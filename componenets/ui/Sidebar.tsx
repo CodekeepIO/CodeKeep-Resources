4c2c201d578577e5a2aebe97e78cb18fffc8d590
Pjijin <Pjijin1@gmail.com>
2020-04-07 19:27:06 +0530

    🐛 Fix: Removed producthunt badge

--- componenets/ui/Sidebar.tsx
+++ componenets/ui/Sidebar.tsx
@@ -52,18 +52,6 @@ const Sidebar = ({ filter, setFilter, clearFilter }) => {
 					))}
 				</ul>
 			</div>
-
-			<a
-				href="https://www.producthunt.com/posts/dev-resources-by-codekeep?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-dev-resources-by-codekeep"
-				target="_blank"
-			>
-				<img
-					src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=191204&theme=light"
-					alt="Dev Resources by Codekeep - #StayHome - Free Resources to up-skill yourself. | Product Hunt Embed"
-					width="250px"
-					height="54px"
-				/>
-			</a>
 		</SidebarStyle>
 	);
 };
